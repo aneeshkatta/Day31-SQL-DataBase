c6185c2c110127a4bf5ce744c7d5b21ebb7515fa
Aneesh <katta.aneesh597@gmail.com>
2022-12-20 16:52:16 +0530

    added uc2

--- CreatePayRollServiceDB.sql
+++ CreatePayRollServiceDB.sql
@@ -1,3 +1,12 @@
 -------UC-01-Creating database---------
 create database Payroll_Service;
 use model
+---------UC-02-Creating table-----------
+create table EmployeePayRoll
+(
+	Id int  identity(1,1)  primary key,
+    Name varchar(255),
+    Address varchar(255),
+    Salary float,
+	StartDate date
+);
