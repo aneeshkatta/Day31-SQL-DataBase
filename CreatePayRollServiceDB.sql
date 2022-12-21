b4b0e861a15157594871528708142e464bde15b4
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-12-21 22:41:25 +0530

    Update CreatePayRollServiceDB.sql

--- CreatePayRollServiceDB.sql
+++ CreatePayRollServiceDB.sql
@@ -1,16 +1,4 @@
 -------UC-01-Creating database---------
 create database Payroll_Service;
-<<<<<<< HEAD
-use model
----------UC-02-Creating table-----------
-create table EmployeePayRoll
-(
-	Id int  identity(1,1)  primary key,
-    Name varchar(255),
-    Address varchar(255),
-    Salary float,
-	StartDate date
-);
-=======
 use Payroll_Service;
->>>>>>> uc1
+
